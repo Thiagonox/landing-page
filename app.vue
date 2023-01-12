7cf22cfda3a7d7c4986a626c4d9962327e02f78c
Thiagonox <thiagogentil499@gmail.com>
2023-01-12 08:36:15 -0300

    Removing ignored file from repository

--- app.vue
+++ app.vue
@@ -1,48 +1,5 @@
 <template>
-  <div data-theme="acid" class="h-screen">
-    <header class="navbar bg-base-100 w-[97%] m-auto">
-      <div class="flex-1">
-        <div class="btn btn-ghost normal-case text-xl ">
-          <p>
-            ラーメン
-          </p>
-        </div>
-      </div>
-      <div class="w-1/3">
-        <ul class="w-full flex justify-between">
-          <li><a class="border-b-2 border-primary cursor-pointer">Home</a></li>
-          <li><a class="cursor-pointer">Menu</a></li>
-          <li><a class="cursor-pointer">Contact Us</a></li>
-        </ul>
-      </div>
-    </header>
-    <main class=" flex pt-4 w-[97%] m-auto">
-      <div class="w-1/2">
-        <h2 class="text-5xl text-primary">ラーメン</h2>
-        <hr class="w-1/3 border-base-content border-t-2 my-4">
-        <div class="text-5xl ">
-          <h2>A BOWLY OF LOVE FROM</h2>
-          <h2 class="text-primary">JAPANESE CUISINE</h2>
-          <h2>FOR YOU</h2>
-        </div>
-        <p class="w-5/6 py-4">
-          Ramen is a traditional Japanese noodle soup. It consists of Chinese wheat noodles served in a meat or fish-based broth & uses toppings such as sliced pork, nori, menma, and scallions.
-        </p>
-        <button class="btn btn-outline">Order Now</button>
-      </div>
-      <div class="w-1/2">
-        <img class="" src="https://o.remove.bg/downloads/59154a5f-ff30-4969-aa86-3b1f680b906b/50c17b32af13c7c9df9fd33d4f41a1a6-removebg-preview.png" alt="">    
-      </div>
-    </main>
-    <footer class="w-[97%] m-auto flex items-center justify-between">
-        <ul class="w-1/3 flex justify-between">
-          <li><a>Instagram</a></li>
-          <li><a>Facebook</a></li>
-          <li><a>Twitter</a></li>
-        </ul>
-        <div class="w-full">
-          <hr class="border-base-content border-t-2 ml-8">
-        </div>
-    </footer>
+  <div>
+    <NuxtWelcome />
   </div>
 </template>
