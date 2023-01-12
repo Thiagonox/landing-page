64c064ac9e2973d8f489d7042e9c9f3994ccb1a5
Thiagonox <thiagogentil499@gmail.com>
2023-01-12 11:16:45 -0300

    update

--- app.vue
+++ app.vue
@@ -16,8 +16,8 @@
         </ul>
       </div>
     </header>
-    <main class=" flex pt-4 w-[97%] m-auto">
-      <div class="w-1/2">
+    <main class=" md:flex pt-4 w-[97%] m-auto">
+      <div class="md:w-1/2 w-full">
         <h2 class="text-5xl text-primary">音楽アルゴリズム</h2>
         <hr class="w-1/3 border-base-content border-t-2 my-4">
         <div class="text-5xl ">
@@ -26,18 +26,18 @@
           <h2>FOR YOU</h2>
         </div>
         <p class="w-5/6 py-4">
-          Japanese robotic music combines technology and tradition, with artists creating unique sounds using machines and software. Japan's innovative culture is reflected in its robotic music scene. Festivals are growing, and Japanese robotic music is a major contributor to the global scene.
+          Robotic music in Japan combines technology and tradition, creating unique sounds. Japan's innovative culture is reflected in its robotic music scene, with festivals growing. Japanese robotic music is a major contributor to the global scene.
         </p>
-        <button class="btn btn-outline">know More</button>
+        <button class="btn btn-outline">Learn More</button>
       </div>
-      <div class="w-1/2">
+      <div class="w-1/2 hidden md:block">
         <img class="" src="https://o.remove.bg/downloads/303748cf-4ae6-4f2e-a86b-e112563e3423/image-removebg-preview.png" alt="">    
       </div>
     </main>
-    <footer class="w-[97%] m-auto flex items-center justify-between">
-        <ul class="w-1/3 flex justify-between">
-          <li><a>Instagram</a></li>
-          <li><a>Facebook</a></li>
+    <footer class="w-[97%] m-auto mt-2 flex items-center justify-between">
+        <ul class="flex">
+          <li class="mr-4"><a>Instagram</a></li>
+          <li class="mr-4"><a>Facebook</a></li>
           <li><a>Twitter</a></li>
         </ul>
         <div class="w-full">
