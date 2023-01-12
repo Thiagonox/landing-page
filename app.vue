43e425e82f65ebf2445de5858140cc5b16982bd9
Thiagonox <thiagogentil499@gmail.com>
2023-01-12 10:54:58 -0300

    update

--- app.vue
+++ app.vue
@@ -1,5 +1,49 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div data-theme="cyberpunk" class="h-screen">
+    <header class="navbar bg-base-100 w-[97%] m-auto">
+      <div class="flex-1">
+        <div class="btn btn-ghost normal-case text-xl ">
+          <p>
+            ロボテューンズ
+          </p>
+        </div>
+      </div>
+      <div class="w-1/3">
+        <ul class="w-full flex justify-between">
+          <li><a class="border-b-2 border-primary cursor-pointer">Home</a></li>
+          <li><a class="cursor-pointer">Menu</a></li>
+          <li><a class="cursor-pointer">Contact Us</a></li>
+        </ul>
+      </div>
+    </header>
+    <main class=" flex pt-4 w-[97%] m-auto">
+      <div class="w-1/2">
+        <h2 class="text-5xl text-primary">音楽アルゴリズム</h2>
+        <hr class="w-1/3 border-base-content border-t-2 my-4">
+        <div class="text-5xl ">
+          <h2>A CHILLING BEAT FROM</h2>
+          <h2 class="text-primary">ROBOTIC MUSIC</h2>
+          <h2>FOR YOU</h2>
+        </div>
+        <p class="w-5/6 py-4">
+          Japanese robotic music combines technology and tradition, with artists creating unique sounds using machines and software. Japan's innovative culture is reflected in its robotic music scene. Festivals are growing, and Japanese robotic music is a major contributor to the global scene.
+        </p>
+        <button class="btn btn-outline">know More</button>
+      </div>
+      <div class="w-1/2">
+        <img class="" src="https://o.remove.bg/downloads/303748cf-4ae6-4f2e-a86b-e112563e3423/image-removebg-preview.png" alt="">    
+      </div>
+    </main>
+    <footer class="w-[97%] m-auto flex items-center justify-between">
+        <ul class="w-1/3 flex justify-between">
+          <li><a>Instagram</a></li>
+          <li><a>Facebook</a></li>
+          <li><a>Twitter</a></li>
+        </ul>
+        <div class="w-full">
+          <hr class="border-base-content border-t-2 ml-8">
+        </div>
+    </footer>
   </div>
 </template>
+
